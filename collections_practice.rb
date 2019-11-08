10a9049d699c439d7c199b5d15085f11b349e082
Kizito Njoku <nkizito57@gmail.com>
2019-11-08 21:15:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,3 +10,17 @@ def sort_array_asc(array)
    end
  end 
 end 
+
+def sort_array_desc(array) 
+
+ array.sort do |a, b|
+   if a < b 
+     -1
+     elsif a > b 
+     1 
+     elsif a == b 
+     0 
+   end
+ end 
+end 
+
