ed7698bb290f27d436a2a45ead48dad4e3befb9d
Kizito Njoku <nkizito57@gmail.com>
2019-11-08 21:13:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,11 @@
 def sort_array_asc 
- array = [3,4,9,1,2,8,7]  
+ array = [3,4,9,1,2,8,7] 
+ array.sort do |a, b|
+   if a < b 
+     -1
+     elsif a > b 
+     1 
+     elsif a == b 
+     0 
+   end
 end 
